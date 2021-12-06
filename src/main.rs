fb81310904c76b2d07feab5303231d9e5d5ff76d
Mateusz Moszczyński <matthew.moszczynski96@gmail.com>
2021-12-06 23:29:21 +0100

    Quit button
    - added functionality to the main exit button in launcher

--- src/main.rs
+++ src/main.rs
@@ -4,5 +4,10 @@ sixtyfps::sixtyfps!(import { MainWindow } from "src/ui/main.60";);
 
 pub fn main() {
     let main_window = MainWindow::new();
+    
+    main_window.on_close({
+        move || std::process::exit(0)
+    });
+
     main_window.run();
 }
