7a3c752b8d302af9dcf913dad615b399b5b49c1a
Mateusz Moszczyński <matthew.moszczynski96@gmail.com>
2022-01-24 00:15:16 +0100

    Add titles
    - add titles retrieved from request

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,10 @@
 sixtyfps::sixtyfps!(import { MainWindow } from "src/ui/main.60";);
+sixtyfps::sixtyfps!(import { NewsData } from "src/ui/newsdata.60";);
 
-use std::collections::HashMap;
+//use std::collections::HashMap;
+use sixtyfps::SharedString;
 use serde::Deserialize;
+
 
 #[derive(Debug, Deserialize)]
 struct DiscussionsIncludesAttributes {
@@ -52,21 +55,31 @@ struct Discussions {
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let resp = reqwest::get("https://project-trains.pl/api/discussions?include=firstPost&filter%5Btag%5D=devlogi&sort=-createdAt&page%5Boffset%5D=0")
         .await?;
-
     let resp_json = resp.json::<Discussions>().await?;
-
+    let mut discussions: Vec<NewsData> = vec![];
     println!("{:#?}", resp_json);
 
-    let main_window = MainWindow::new();
+    for i in 0..resp_json.data.len() {
+        let mut disc = NewsData {
+            title: SharedString::from(resp_json.data[i].attributes.title.clone()),
+            //image: SharedString::from("../assets/devlog.jpg"),
+        };
 
+        discussions.push(disc);
+    }
+
+    let discussions_model = std::rc::Rc::new(sixtyfps::VecModel::from(discussions));
+
+    let main_window = MainWindow::new();
+    main_window.set_news(sixtyfps::ModelHandle::new(discussions_model.clone()));
     main_window.on_blog_redirect({
         move || { open::that("https://project-trains.pl/").unwrap(); }
     });
-
     main_window.on_close({
         move || { std::process::exit(0); }
     });
 
     main_window.run();
+
     Ok(())
 }
