4354e98e2a7f40acbb02e81a40793461fd92ebdc
Mateusz Moszczyński <matthew.moszczynski96@gmail.com>
2022-02-16 19:25:16 +0100

    Add news listing
    - add listing news with titles and corresponding images
    - rename featured image property to "featured"
    - update function parse_img for pasing RGBA format images
    - remove old commented code

--- src/lib.rs
+++ src/lib.rs
@@ -11,9 +11,20 @@ pub async fn load_img(url: &str) -> (Vec<u8>, u32, u32) {
 pub fn parse_img(img_bytes: Vec<u8>, w: u32, h: u32) -> sixtyfps::Image {
     let mut pixel_buffer = sixtyfps::SharedPixelBuffer::new(w as usize, h as usize);
     let bytes = pixel_buffer.make_mut_bytes();
+    
+    // print!("w * h: {0}\n", w * h);
+    // print!("img_bytes: {0}\n", img_bytes.len());
+    // print!("bytes: {0}\n", bytes.len());
+    // print!("\n");
+    
     for i in 0..img_bytes.len() {
+        // Index out of bounds check
+        // if i >= bytes.len() || i >= img_bytes.len() {
+        //     break;
+        // }
+
         bytes[i] = img_bytes[i];
     }
 
-    sixtyfps::Image::from_rgb8(pixel_buffer)
+    sixtyfps::Image::from_rgba8(pixel_buffer)
 }
--- src/main.rs
+++ src/main.rs
@@ -7,55 +7,47 @@ use serde::Deserialize;
 use sixtyfps::SharedString;
 use std::time::Duration;
 
-// #[derive(Debug, Deserialize)]
-// struct DiscussionsIncludesAttributes {
-//     contentHtml : String,
-// }
+#[derive(Debug, Deserialize)]
+struct DiscussionsIncludesAttributes {
+    featuredImage : String,
+}
 
-// #[derive(Debug, Deserialize)]
-// struct DiscussionsIncludes {
-//     id : String,
-//     attributes : DiscussionsIncludesAttributes,
-// }
+#[derive(Debug, Deserialize)]
+struct DiscussionsIncludes {
+    id : String,
+    attributes : DiscussionsIncludesAttributes,
+}
 
-// #[derive(Debug, Deserialize)]
-// struct DiscussionsDataRelationshipsFirstPostData {
-//     id : String
-// }
+#[derive(Debug, Deserialize)]
+struct DiscussionsDataAttributes {
+    title : String,
+    shareUrl : String,
+}
 
-// #[derive(Debug, Deserialize)]
-// struct DiscussionsDataRelationshipsFirstPost {
-//     data : DiscussionsDataRelationshipsFirstPostData
-// }
+#[derive(Debug, Deserialize)]
+struct DiscussionsData {
+    id : String,
+    attributes : DiscussionsDataAttributes,
+}
 
-// #[derive(Debug, Deserialize)]
-// struct DiscussionsDataRelationships {
-//     firstPost : DiscussionsDataRelationshipsFirstPost
-// }
+#[derive(Debug, Deserialize)]
+struct Discussions {
+    data : Vec<DiscussionsData>,
+    included : Vec<DiscussionsIncludes>
+}
 
-// #[derive(Debug, Deserialize)]
-// struct DiscussionsDataAttributes {
-//     title : String,
-//     shareUrl : String
-// }
-
-// #[derive(Debug, Deserialize)]
-// struct DiscussionsData {
-//     id : String,
-//     attributes : DiscussionsDataAttributes,
-//     relationships : DiscussionsDataRelationships
-// }
-
-// #[derive(Debug, Deserialize)]
-// struct Discussions {
-//     data : Vec<DiscussionsData>,
-//     included : Vec<DiscussionsIncludes>
-// }
+struct Discussion {
+    title : String,
+    image_buffer : Vec<u8>,
+    width: u32,
+    height: u32
+}
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let main_window = MainWindow::new();
     let handle_weak = main_window.as_weak();
+    let mut discussions: Vec<Discussion> = vec![];
 
     main_window.on_blog_redirect({
         move || {
@@ -73,36 +65,56 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         // delay for testing
         std::thread::sleep(Duration::new(2, 0));
 
+        let disc_resp = reqwest::get("https://project-trains.pl/api/discussions?include=blogMeta&filter[q]=is:blog+tag:devlogi&sort=-createdAt").await;
+        let disc_json = disc_resp.unwrap().json::<Discussions>().await.unwrap();
+        println!("{:#?}", disc_json);
+
+        for i in 0..disc_json.data.len() {
+            let img_url : &str = &disc_json.included[i].attributes.featuredImage;
+            let (img_buffer, w, h) = load_img(img_url).await;
+            // let (img_buffer, w, h) = load_img("https://picsum.photos/800/450").await;
+
+            let disc = Discussion {
+                title: disc_json.data[i].attributes.title.clone(),
+                image_buffer: img_buffer,
+                width: w,
+                height: h
+            };
+
+            discussions.push(disc);
+        }
+
+        update_discussions(handle_weak.clone(), discussions);
+
         let (img, w, h) = load_img("https://picsum.photos/800/450").await;
-        update_featured(handle_weak.clone(), img, w, h);       
+        update_featured(handle_weak.clone(), img, w, h);
     });
 
-    // let resp = reqwest::get("https://project-trains.pl/api/discussions?include=firstPost&filter%5Btag%5D=devlogi&sort=-createdAt&page%5Boffset%5D=0")
-    // .await?;
-    // let resp_json = resp.json::<Discussions>().await?;
-    let mut discussions: Vec<NewsData> = vec![];
-    // println!("{:#?}", resp_json);
-
-    // for i in 0..resp_json.data.len() {
-    // let mut disc = NewsData {
-    // title: SharedString::from(resp_json.data[i].attributes.title.clone()),
-    //image: SharedString::from("../assets/devlog.jpg"),
-    // };
-
-    // discussions.push(disc);
-    // }
-
-    let discussions_model = std::rc::Rc::new(sixtyfps::VecModel::from(discussions));
-
-    main_window.set_news(sixtyfps::ModelHandle::new(discussions_model.clone()));
     main_window.run();
-
     Ok(())
+}
+
+fn update_discussions(handle: sixtyfps::Weak<MainWindow>, discussions: Vec<Discussion>) {
+    handle.upgrade_in_event_loop(move |handle| {
+        let mut news_data: Vec<NewsData> = vec![];
+
+        for i in 0..discussions.len() {
+            let news = NewsData {
+                title: SharedString::from(discussions[i].title.clone()),
+                cover: parse_img(discussions[i].image_buffer.to_vec(), discussions[i].width, discussions[i].height)
+            };
+
+            news_data.push(news);
+        }
+
+        let news_model = std::rc::Rc::new(sixtyfps::VecModel::from(news_data));
+        handle.set_news(sixtyfps::ModelHandle::new(news_model.clone()));
+    });
 }
 
 fn update_featured(handle: sixtyfps::Weak<MainWindow>, image: Vec<u8>, w: u32, h: u32) {
     handle.upgrade_in_event_loop(move |handle| {
         let img = parse_img(image, w, h);
-        handle.set_image(img);
+        handle.set_featured(img);
     });
 }
